97b85224a568935b6e0e77f5cd84136990356423
조형배 <pwjhb@naver.com>
2020-06-04 16:55:31 +0900

    Create About.vue

---
+++ test1/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  <Cho title="About Title" :name1= "name"></Cho>
+  </div>
+</template>
+
+<script>
+import Cho from '@/components/cho.vue';
+
+export default {
+  components:{
+    Cho
+  },
+  data(){ 
+        return{
+
+            name: 'KIM'
+        }
+
+    }
+}
+</script>
