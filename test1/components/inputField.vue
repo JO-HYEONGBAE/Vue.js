2c008d8665467a9e266e70710a899757d84fa385
조형배 <pwjhb@naver.com>
2020-06-04 16:55:09 +0900

    Create inputField.vue

---
+++ test1/components/inputField.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <label for="">Name</label>
+         <input type="text" v-bind:value="name" style="padding:30px; border:2px solid blue"
+         v-on:input="updateName">
+         <br>
+         <button>Submit</button>
+</div>
+    
+</template>
+
+<script>
+export default {
+    props:{
+        name:{
+            type:String 
+        }
+    },
+    methods:{
+        updateName(e){
+            console.log(e.target.value);
+            this.$emit('update-name',e.target.value);
+             this.$emit('update-name2',e.target.value);
+
+        }
+
+    }
+}
+</script>
