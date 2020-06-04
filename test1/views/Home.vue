81f111d2a3744d17e86cd2802f8b0124359e773a
조형배 <pwjhb@naver.com>
2020-06-04 16:55:41 +0900

    Create Home.vue

---
+++ test1/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1> 
+      This is Home page
+    </h1>
+    <form>
+      <InputField :name="name" @update-name="updateName"  @update-name2="updateName"></InputField>
+    </form>
+    {{name}}
+
+    <!-- <p>
+      {{name}}
+    </p>
+    <Cho id="s" title="Home Title"></Cho> -->
+  </div>
+</template>
+
+<script>
+// import Cho from '@/components/cho.vue';
+import InputField from '@/components/inputField.vue';
+export default {
+  // components:{ 
+  //   Cho
+  // },
+  components:{
+    InputField
+  },
+  data (){
+    return{
+      name:"s"
+    }
+  },
+  methods:{
+    updateName(name){
+      this.name=name ;
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+
+  color:red
+}
+#s{
+  color:blue
+}
+</style>
