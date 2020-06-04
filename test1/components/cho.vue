44ec3f86942dbbd88881f62f75418fb476128260
조형배 <pwjhb@naver.com>
2020-06-04 16:54:52 +0900

    Create cho.vue

---
+++ test1/components/cho.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>{{title}}</p>
+        <h1>
+            {{name1}}
+        </h1>
+        <button type="button" v-on:click="updateName">버튼</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props:['title','name1'],
+    data(){ 
+        return{
+
+            name: 'KIM'
+        }
+
+    },
+
+    methods:{
+        updateName(){
+            this.name='CHO'
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
